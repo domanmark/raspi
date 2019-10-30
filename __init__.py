956d138eff7d43171b3c65e63ce5b5d943e62203
domanmark <mpd.github@gmail.com>
2019-10-29 20:58:44 -0400

    add news api

--- __init__.py
+++ __init__.py
@@ -2,9 +2,10 @@ import requests
 import json
 import sys
 import time
+import random
 from flask import Flask, render_template
 
-# get DarkSky API key stored outside the app source code
+# read in private configurations file
 try:
     with open('api_config.json', 'r') as f:
         keys = json.load(f)
@@ -19,8 +20,22 @@ BING_MAPS_KEY = keys['BING_MAPS_KEY']
 DIRECTION_WAYPOINTS = keys['DIRECTION_WAYPOINTS']
 LAT = keys['LATITUDE']
 LONG = keys['LONGITUDE']
+NEWS_API_KEY = keys['NEWS_API_KEY']
 
 app = Flask(__name__)
+
+@app.route("/news")
+def news():
+    news = requests.get("https://newsapi.org/v2/top-headlines?country=us&apiKey={}".format(
+        NEWS_API_KEY
+        )
+    )
+    news_json = json.loads(news.text)
+    headlines = []
+    for article in news_json["articles"]:
+        headlines.append(article["title"])
+    random_headlines = random.sample(headlines, 3)
+    return '<br><br>'.join(random_headlines)
 
 @app.route("/weather")
 def weather():
@@ -33,10 +48,12 @@ def weather():
     weather_json = json.loads(weather.text)
 
     return (time.ctime()
-            + "<br></br>Current temp: "
+            + "<br></br>Current weather: "
             + str(weather_json["currently"]["temperature"])
             + "&#176;"
-            + "<br></br>Today's forecast: "
+            + " and "
+            + str(weather_json["currently"]["summary"])
+            + "<br></br>Forecast: "
             + str(weather_json["hourly"]["summary"])
             )
 
@@ -54,7 +71,8 @@ def directions():
     )
     directions_json = json.loads(directions.text)
     drive_time = directions_json['resourceSets'][0]['resources'][0]['travelDurationTraffic'] / 60 # time in mins
-    return "time to work: {:.2f} mins".format(drive_time)
+
+    return "Time to work: {:.2f} mins".format(drive_time)
 
 @app.route("/dashboard")
 def dashboard():
